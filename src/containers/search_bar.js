ddd334e1e6ef4acb66022b58518b049d9169079a
Winkeltje84 <bwinkel@gmail.com>
2017-06-22 01:20:03 +0200

    created functionality that sends changes in the input field to the onInputChange function

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+  // this constructor sets the search state to an empty string at first
+  // and defines it as 'term'
+
   render() {
       return (
         <form className="input-group">
-          <input />
-            <span className="input-group-btn">
-              <button type="submit" className="btn btn-secondary">Submit</button>
-            </span>
+          <input
+            placeholder="Pick a city and see it's 5-day weather forecast!"
+            className="form-control"
+            value={ this.state.term }
+            onChange={this.onInputChange}
+          />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-secondary">Submit</button>
+          </span>
         </form>
       )
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value)
   }
 }
 
