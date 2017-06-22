f8f7823d58cfcaf964af9e41367700c0c78a03cb
Winkeltje84 <bwinkel@gmail.com>
2017-06-22 13:50:11 +0200

    started action that creates a url that would get us the wanted weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
-const API_KEY = "501cf5432e6aac36ac79822d3b928db7";
+const API_KEY = '501cf5432e6aac36ac79822d3b928db7';
+const API_CALL_ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+// above taken from http://samples.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=b1b15e88fa797225412429c1c50c122a1
+// taken out:
+        // the 'q=London,us'    -- this will be added by the search bar & added lateron
+        // the '&mode=xml'      -- because we want JSON
+
+export const FIND_WEATHER = 'FIND_WEATHER'
+// this time we create a constant that we set to 'FIND_WEATHER' to prevent
+// the possibility of future typo's
+
+export function findWeather(city) {
+  const url = `${API_CALL_ROOT_URL}&${city},nl`;
+
+  return {
+    type: FIND_WEATHER,
+    
+  }
+}
