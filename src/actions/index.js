d8e7c41c964b04bfc36df8eeddf779b3584af245
Winkeltje84 <bwinkel@gmail.com>
2017-06-22 14:02:05 +0200

    using Axios to create an Ajax get request which will set up a promise to get us some weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const API_KEY = '501cf5432e6aac36ac79822d3b928db7';
 const API_CALL_ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 // above taken from http://samples.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=b1b15e88fa797225412429c1c50c122a1
@@ -11,9 +12,13 @@ export const FIND_WEATHER = 'FIND_WEATHER'
 
 export function findWeather(city) {
   const url = `${API_CALL_ROOT_URL}&${city},nl`;
+  const request = axios.get(url);
+  // DOCUMENTATION: https://www.npmjs.com/package/axios
+  // axios: does an ajax request (get) to the url and returns a promise
+  // that promise we attach to the payload of this actioncreator
 
   return {
     type: FIND_WEATHER,
-    
+    payload: request
   }
 }
