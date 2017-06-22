46c5ea97d44003c70b537261c7f43d63437aa293
Winkeltje84 <bwinkel@gmail.com>
2017-06-22 11:14:01 +0200

    onInputChange now updates the state of the search "term"

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,29 +4,37 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: 'test'};
+
+    this.onInputChange = this.onInputChange.bind(this)
+    // above binds this to the onInputChange function since the function
+    // originally doesn't know about 'this'
   }
   // this constructor sets the search state to an empty string at first
   // and defines it as 'term'
 
-  render() {
-      return (
-        <form className="input-group">
-          <input
-            placeholder="Pick a city and see it's 5-day weather forecast!"
-            className="form-control"
-            value={ this.state.term }
-            onChange={this.onInputChange}
-          />
-          <span className="input-group-btn">
-            <button type="submit" className="btn btn-secondary">Submit</button>
-          </span>
-        </form>
-      )
-  }
-
   onInputChange(event) {
     console.log(event.target.value)
+
+    this.setState( { term: event.target.value })
+    // because 'this' has been binded to the onInputChange function in th
+    // constructor, we can now use setState to set the new state of 'term'
+  }
+
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          placeholder="Pick a city and see it's 5-day weather forecast!"
+          className="form-control"
+          value={ this.state.term }
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
   }
 }
 
